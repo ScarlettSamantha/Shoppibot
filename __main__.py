ce5c89454417c3e31252a1e227c47a809ee8a8cc
Scarlett Verheul <scarlett@arcane-tech.nl>
2017-12-13 12:19:49 +0100

    [TASK] Added typehinting and docs to the functions

--- __main__.py
+++ __main__.py
@@ -1,66 +1,137 @@
 # build-ins
 import argparse
 import logging
+
 # dep: requests
 import requests
+
 # ./config.py
 import config
 
 
 class Shoppimon:
-    
     @staticmethod
-    def get_headers(data):
+    def get_headers(data: dict) -> dict:
         return {**{
             "Accept": "application/json",
             "content-type": "application/json",
         }, **data}
 
     @classmethod
-    def offline(cls, client_id, secret, website_id):
+    def offline(cls, client_id: str, secret: str, website_id: str) -> None:
+        """Tell shoppinmon to disable the testing.
+
+        Args:
+           client_id (str): The client id can be found at https://app.shoppimon.com/user/api
+           secret (str): The client secret can be found at https://app.shoppimon.com/user/api 
+           website_id (str): The website id can be found using the web_websites call.
+        """
         cls(client_id, secret).disable_testing(website_id)
 
     @classmethod
-    def online(cls, client_id, secret, website_id):
+    def online(cls, client_id: str, secret: str, website_id: str) -> None:
+        """Tell shoppinmon to re-enable the testing (to 15min).
+       
+       Args:
+           client_id (str): The client id can be found at https://app.shoppimon.com/user/api
+           secret (str): The client secret can be found at https://app.shoppimon.com/user/api 
+           website_id (str): The website id can be found using the web_websites call.
+       """
         cls(client_id, secret).enable_testing(website_id)
 
     @classmethod
-    def get_shops(cls, client_id, secret):
+    def get_shops(cls, client_id: str, secret: str) -> dict:
+        """Will return the websites linked to the customer
+ 
+         Args:
+             client_id (str): The client id can be found at https://app.shoppimon.com/user/api
+             secret (str): The client secret can be found at https://app.shoppimon.com/user/api 
+         Returns:
+             Dict<int, str>: A dict with the shop id as key and the name of the shop as value str
+        
+        """
         r = {}
         for account in cls(client_id, secret).get_account_details()['_embedded']['account']:
             r[account['id']] = account['name']
         return r
 
     @classmethod
-    def get_website_for_account(cls, client_id, secret, customer_id):
+    def get_website_for_account(cls, client_id: str, secret: str, customer_id: str) -> dict:
+        """Will return the websites linked to the customer
+        
+        Args:
+            client_id (str): The client id can be found at https://app.shoppimon.com/user/api
+            secret (str): The client secret can be found at https://app.shoppimon.com/user/api 
+            customer_id (str): The customer id can be found using the get_shops call.
+        Returns:
+            Dict<int, dict>: A dict with the shop id as key and the id, name, url as keys in a sub dict.
+
+        """
         r = {}
         for domain in cls(client_id, secret).get_websites(customer_id)['_embedded']['website']:
             r[domain['id']] = {'id': domain['id'], 'name': domain['name'], 'url': domain['base_url']}
         return r
 
-    def __init__(self, client_id, secret):
+    def __init__(self, client_id: str, secret: str) -> None:
+        """Constructor will load the given args into their properties.
+        
+        Args:
+            client_id (str): The client id can be found at https://app.shoppimon.com/user/api
+            secret (str): The client secret can be found at https://app.shoppimon.com/user/api
+        """
         self.client_id = client_id
         self.secret = secret
         self.bearer = None
 
-    def post_request(self, endpoint, request, add_key=True):
+    def post_request(self, endpoint: str, request: dict, add_key=True) -> dict:
+        """Will send a post request to the given endpoint with the given payload formatted as json.
+
+        Args:
+            endpoint (str): The endpoint to talk to. 
+            request (dict): What you want to send the endpoint (will be converted to json)
+            add_key (bool): If true it will add the authorization bearer if false not this 
+                is due to the key request also being a post.
+        Returns:
+            A dict with the decoded response json
+        """
         response = requests.post(endpoint, json=request, headers=self.get_headers(
             {'Authorization': "Bearer " + self.get_key()} if add_key else {}))
         logging.debug(response.text)
         return response.json()
 
-    def get_request(self, endpoint):
+    def get_request(self, endpoint) -> dict:
+        """:obj:`dict`: Will perform a get request
+        
+        Args:
+            endpoint (str): What endpoint to perform the operation against.
+        
+        Returns:
+            Dict with the parsed json.
+        """
         response = requests.get(endpoint, headers=self.get_headers({'Authorization': "Bearer " + self.get_key()}))
         logging.debug(response.text)
         return response.json()
 
-    def patch_request(self, endpoint, payload):
+    def patch_request(self, endpoint: str, payload: dict) -> dict:
+        """Will send a patch request to the given endpoint with the given payload formatted as json.
+        
+        Args:
+            endpoint (str): The endpoint to talk to. 
+            payload (dict): What you want to send the endpoint (will be converted to json)
+        Returns:
+            A dict with the decoded response json
+        """
         response = requests.patch(endpoint, json=payload,
-                              headers=self.get_headers({'Authorization': "Bearer " + self.get_key()}))
+                                  headers=self.get_headers({'Authorization': "Bearer " + self.get_key()}))
         logging.debug(response.text)
         return response.json()
 
-    def fetch_key(self):
+    def fetch_key(self) -> None:
+        """Will fetch the key from the shoppimon server and cache it in a local property
+        
+        Returns:
+            None
+        """
         r = self.post_request(config.ENDPOINT_OAUTH, {
             "grant_type": "client_credentials",
             "client_id": self.client_id,
@@ -68,24 +139,54 @@ class Shoppimon:
         }, False)
         self.bearer = r['access_token']
 
-    def get_key(self):
-        """
-        :return: str 
+    def get_key(self) -> str:
+        """This method will check the cache for a key and if one is not found fetch and cache it.
+        
+        Returns:
+            Str: Will Return the ether cached or fetched key.
         """
         if self.bearer is None:
             self.fetch_key()
         return self.bearer
 
-    def get_account_details(self):
+    def get_account_details(self) -> dict:
+        """Will fetch the account details this contains the customers beneath the account.
+        
+        Returns:
+            Dict: The dictionary contains the decoded json response text.
+        """
         return self.get_request(config.ENDPOINT_USER_INFO)
 
-    def get_websites(self, customer_id):
+    def get_websites(self, customer_id: str) -> dict:
+        """Will fetch the websites connected to a customer.
+        
+        Args:
+            customer_id (str): The customer id which can be gotten from the get_account_details funciton.
+
+        Returns:
+            Dict: containing the decoded JSON.
+        """
         return self.get_request(config.ENDPOINT_WEBSITES % customer_id)
 
-    def disable_testing(self, website_id):
+    def disable_testing(self, website_id) -> dict:
+        """Will instruct shoppimon to never test a given website.
+        
+        Args:
+            website_id (str): The shoppimon id of a website, can be found using get_websites 
+
+        Returns:
+            Dict: The decoded response json.
+        """
         return self.patch_request(config.ENDPOINT_WEBSITE % website_id, {"active": False})
 
-    def enable_testing(self, website_id):
+    def enable_testing(self, website_id) -> dict:
+        """Will re-enable testing for a given store, and set it to every 15 min.
+        
+        Args:
+            website_id (str): The shoppimon id of a website, can be found using get_websites 
+        Returns:
+            Dict: The decoded json.
+        """
         return self.patch_request(config.ENDPOINT_WEBSITE % website_id, {"active": True})
 
 
@@ -131,5 +232,6 @@ if __name__ == "__main__":
         for index, value in Shoppimon.get_shops(config.CLIENT_ID, config.CLIENT_SECRET).items():
             print("id: %s | name: %s" % (index, value))
     elif args.action == 'websites':
-        for index, website in Shoppimon.get_website_for_account(config.CLIENT_ID, config.CLIENT_SECRET, config.WEBSITE_ID).items():
+        for index, website in Shoppimon.get_website_for_account(config.CLIENT_ID, config.CLIENT_SECRET,
+                                                                config.WEBSITE_ID).items():
             print("website-id: %s | name: %s | url: %s" % (website['id'], website['name'], website['url']))
